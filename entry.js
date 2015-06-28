94dfdc19be40f8422a33793cc7543f587921bd32
Mike Chelen <michael.chelen@gmail.com>
2015-06-28 11:58:57 -0400

    simplify example

--- entry.js
+++ entry.js
@@ -1,12 +1,6 @@
-var $ = require('jquery');
+var $ = require("jquery");
 $.mobile = require('jquery-mobile');
-
-jqVersion = $().jquery;
-$('#jq-version').text(jqVersion);
-
-// custom button click handler to demonstrate working $.mobile function
-$(document).on('click', 'button', function() {
-  var link = $(this).attr("data-foo");
-  console.log('running $.mobile.changePage("'+link+'")');
-  $.mobile.changePage(link);
-});
+ 
+$(document).ready(function() {
+  console.log("hello world");
+}); 
